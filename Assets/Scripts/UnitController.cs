d697e1233a56e81a292f9a68b28c071960435ec9
Benoki9 <benoki09@gmail.com>
2023-11-21 18:27:39 +0100

    partially created level, added unit prefab

---
+++ Assets/Scripts/UnitController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Animator anim;
+    private RaycastHit hit;
+    private MySelectable ms;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
