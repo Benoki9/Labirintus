56c736b2a2e9230ae09508536d76a3ac0356769f
Benoki9 <benoki09@gmail.com>
2023-11-28 17:28:35 +0100

    Destinatio added :)

--- Assets/Scripts/UnitController.cs
+++ Assets/Scripts/UnitController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class UnitController : MonoBehaviour
 {
-    private NavMeshAgent agent;
+    private UnityEngine.AI.NavMeshAgent agent;
     private Animator anim;
     private RaycastHit hit;
     private MySelectable ms;
@@ -12,12 +12,22 @@ public class UnitController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        ms = GetComponent<MySelectable>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach(var soldier in MySelectable.allMySelectables){
+            if(soldier.selected){
+                if(Input.GetMouseButtonDown(0)){
+                    if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)){
+                        agent.destination = hit.point;
+                    }
+                }
+            }
+        }
     }
 }
